f7bcb82b5e97293d0abf934091e327f3673072ea
rohitkadu <rohitkadu3102@gmail.com>
2026-08-16 14:38:49 +0530

    fix: health and metrics endpoint

--- src/main/java/com/rohitfi/config/SecurityConfig.java
+++ src/main/java/com/rohitfi/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
                     "/swagger-ui/**",
                     "/swagger-ui.html",
                     "/v3/api-docs/**",
-                    "/v3/api-docs.yaml"
+                    "/v3/api-docs.yaml",
+                    "/actuator/**"
                 ).permitAll()
                 // Manager only
                 .requestMatchers("/api/manager/**").hasAuthority("ROLE_MANAGER")
