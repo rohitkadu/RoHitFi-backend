4e45de4491d6ba9a36a1bc1616c505132ef71396
rohitkadu <rohitkadu3102@gmail.com>
2026-08-16 15:02:13 +0530

    fix: indents

--- src/main/java/com/rohitfi/RohitFiApplication.java
+++ src/main/java/com/rohitfi/RohitFiApplication.java
@@ -2,12 +2,10 @@ package com.rohitfi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @EnableScheduling
 @EnableAsync
 public class RohitFiApplication {
